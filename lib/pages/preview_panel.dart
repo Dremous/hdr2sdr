b8d93dccab564093ee6ae762d233e47b823093b1
Dremous <l1950767269@outlook.com>
2026-07-05 21:13:17 +0800

    feat: 添加预览面板页面

---
+++ lib/pages/preview_panel.dart
@@ -0,0 +1,41 @@
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/convert_provider.dart';
+
+class PreviewPanel extends StatelessWidget {
+  const PreviewPanel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ConvertProvider>(
+      builder: (context, provider, _) {
+        final frame = provider.previewFrame;
+        return Container(
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: frame != null
+              ? Image.memory(
+                  frame,
+                  fit: BoxFit.contain,
+                  gaplessPlayback: true,
+                )
+              : Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.movie_outlined,
+                          size: 64, color: Colors.grey[600]),
+                      const SizedBox(height: 12),
+                      Text('选择文件后显示预览',
+                          style: TextStyle(color: Colors.grey[500])),
+                    ],
+                  ),
+                ),
+        );
+      },
+    );
+  }
+}
