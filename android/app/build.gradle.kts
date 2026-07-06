5f3fb25a38e1093c4de69391a9b6a9a7936226b1
Dremous <l1950767269@outlook.com>
2026-07-06 12:53:39 +0800

    fix: android compileSdk 提升到 34，desktop_drop 插件需要 SDK 34+ 的依赖

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.hdr2sdr"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 34  // desktop_drop 等插件需要 compileSdk 34
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
